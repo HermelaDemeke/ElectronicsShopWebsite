b938f89aab46cf0ff02bb97283be73e912320635
HermelaDemeke <bhmd0595@gmail.com>
2024-06-02 18:51:48 +0300

    linter config

--- assets/js/navigate.js
+++ assets/js/navigate.js
@@ -1,10 +1,8 @@
-function showSidebar(){
-    const sidebar=document.querySelector('.sidebar');
-    sidebar.style.display='flex';
-
-}
-function hideSidebar(){
-    const sidebar=document.querySelector('.sidebar');
-    sidebar.style.display='none';
-
-}
+document.getElementById('nav').onclick = function showSidebar() {
+  const sidebar = document.querySelector('.sidebar');
+  sidebar.style.display = 'flex';
+};
+document.getElementById('nav2').onclick = function hideSidebar() {
+  const sidebar = document.querySelector('.sidebar');
+  sidebar.style.display = 'none';
+};
